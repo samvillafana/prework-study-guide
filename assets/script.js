e84cd35098bb4c4c5fad5d465eb4e6c1ad7cab39
Samuel <samuel@samuelvafanamac.myfiosgateway.com>
2022-11-08 00:05:11 -0500

    for loops

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,8 @@
-var topics = [ 'HTML', 'CSS', 'Git','JavaScript'];
 
-// for (var x = 0; x <topics.length; x++ ){
+// var shapes =["triangle", "square", "pentagon", "circle"];
 
-//     console.log(topics[x]);
-// }
+var studentInfo =["Lu", "54", "true"];
 
-
-if (topics === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topics === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topics === 'Git') {
- console.log("Let's study Git!");
-} else if (topics === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
+for(var x =0; x < studentInfo.length; x++){
+    console.log(studentInfo[x]);
 }
